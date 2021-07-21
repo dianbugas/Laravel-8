6f3fbe982f2fed75f94f56cb0c637af06221ae8a
dianynf <dianbugas@gmail.com>
2021-07-21 23:01:24 +0700

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.main')
+
+@section('container')
+    <article>
+        <h2>Judul</h2>
+        <h5>Author</h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nam amet laudantium, sit dolorum eligendi
+            veniam. Porro perferendis tempora illum soluta. Illum quam cum, voluptate libero corporis at nulla
+            necessitatibus veniam rem eos placeat, dolorum repellat praesentium quaerat delectus quisquam ratione explicabo
+            minima sint. Totam sunt illum repudiandae similique, voluptatem perspiciatis, id odit facilis, beatae corrupti
+            nulla soluta. Rerum eveniet eaque neque laudantium mollitia ducimus illo fugiat ab recusandae, minima quasi
+            exercitationem aut quaerat voluptatem ea maiores nisi. Eaque soluta similique, aliquid hic esse itaque ab. Iusto
+            consectetur unde totam libero delectus voluptatem eius facere animi natus nam, nobis quas.</p>
+    </article>
+    <a href="/blog"></a>
+@endsection
